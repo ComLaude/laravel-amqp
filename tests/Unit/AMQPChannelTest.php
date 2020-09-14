6bfe8fb8cfe038fe900dd1151f5583b51de9de68
David Krizanic <david@demys.com>
2020-09-14 14:48:06 +0100

    Adding more tests.

--- tests/Unit/AMQPChannelTest.php
+++ tests/Unit/AMQPChannelTest.php
@@ -21,7 +21,16 @@ class AMQPChannelTest extends BaseTest
     {
         parent::setUp();
 
-        $this->master = AmqpChannel::create( (array) $this->properties, Array( 'queue' => 'testing' ) );
+        $this->master = AmqpChannel::create( (array) $this->properties, [
+            'queue' => 'testing',
+            'connect_options' => ['heartbeat' => 2],
+            'bindings' => [
+                [
+                    'queue'    => 'testing',
+                    'routing'  => 'example.route.key',
+                ],
+            ],
+        ] );
         $this->channel = $this->master->getChannel();
         $this->connection = $this->master->getConnection();
     }
@@ -46,13 +55,43 @@ class AMQPChannelTest extends BaseTest
     {
         $message = new AMQPMessage('Test message');
         
-        $this->master->publish('empty.target', $message);
+        $this->master->publish('example.route.key', $message);
 
         $object = $this;
         $master = $this->master;
 
         $this->master->consume(function($consumedMessage) use ($message, $object, $master) {
             $object->assertEquals($consumedMessage->body, $message->body);
+            $master->acknowledge($consumedMessage);
+        });
+    }
+
+    public function testPublishToChannelAndConsumeDelayed()
+    {
+        $message = new AMQPMessage('Test message');
+        
+        $this->master->publish('example.route.key', $message);
+
+        $object = $this;
+        $master = $this->master;
+
+        // This will hit a heartbeat missed exception but recover from it
+        $this->master->consume(function($consumedMessage) use ($message, $object, $master) {
+            $object->assertEquals($consumedMessage->body, $message->body);
+            sleep(5);
+            $master->acknowledge($consumedMessage);
+        });
+
+        $message2 = new AMQPMessage('Test message 2');
+        $this->master->publish('example.route.key', $message2);
+
+        // This should receive the previously crashed message auto-acknowledge it and proceed to the next one
+        // Because we did not explicitly acknowledge the consumed message here this is a good test of the
+        // automatic acknowledgement of the duplicated message we expect to receive
+        $this->master->consume(function($consumedMessage){});
+        // The second consume should contain the expected second message
+        $this->master->consume(function($consumedMessage) use ($message2, $object, $master) {
+            $object->assertEquals($consumedMessage->body, $message2->body);
             $master->acknowledge($consumedMessage);
         });
     }
